f350aabb1d2d39052908b75fa0a3d2c4a97a70a8
YNC Consulting Inc <yncconsultinginc@gmail.com>
2019-12-10 10:27:32 -0500

    fix bug

--- backend/server/fcc/helper.ts
+++ backend/server/fcc/helper.ts
@@ -49,59 +49,62 @@ export default class Helper {
 
   private static proxies: string[] = [];
 
-  private static fetchAndSave = async (
+  private static fetchAndSave = (
     url: string,
     fccidKey: string,
     repDateVal: string,
     proessFn: (filepath: string, uri: string, fccidKey: string, repDateVal: string) => FCCResult,
     proxyUrl: string
   ) => {
-    const file = url.split("/").pop() || "dummy.pdf";
-    const downloadPath = path.resolve(os.tmpdir());
-    const downloadFile = path.resolve(downloadPath, file);
-    console.log("Downloading file to:", downloadFile);
-
-    const referer = url.replace(/\.pdf$/, "");
-
-    console.log("use proxy, refer", proxyUrl, referer);
-    const options = {
-      method: "GET",
-      uri: url,
-      // body: request.postData,
-      // headers: request.headers,
-      headers: {
-        ":authority": "fccid.io",
-        ":method": "GET",
-        "Content-type": "applcation/pdf",
-        Referer: referer,
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-      },
-      // encoding: null,
-      proxy: proxyUrl,
-    };
-
-    const writable = fs.createWriteStream(downloadFile, { flags: "w+" });
-    const stream = await request
-      .get(options)
-      .on("response", (response: any) => {
-        console.log("response code on download:", response.statusCode);
-        if (response.statusCode >= 500) {
-          console.log(response.statusCode, " Server error", options.uri);
-        }
-      })
-      .on("error", (err: any) => {
-        console.log("err while requesting for pdf");
-        Helper.proxies = Helper.proxies.filter(x => x !== proxyUrl);
-      })
-      .pipe(writable)
-      .on("error", (err: any) => {
-        console.log("err while writing pdf");
-      });
-
     return new Promise<FCCResult>((res, rej) => {
+      const file = url.split("/").pop() || "dummy.pdf";
+      const downloadPath = path.resolve(os.tmpdir());
+      const downloadFile = path.resolve(downloadPath, file);
+      console.log("Downloading file to:", downloadFile);
+
+      const referer = url.replace(/\.pdf$/, "");
+
+      console.log("use proxy, refer", proxyUrl, referer);
+      const options = {
+        method: "GET",
+        uri: url,
+        // body: request.postData,
+        // headers: request.headers,
+        headers: {
+          ":authority": "fccid.io",
+          ":method": "GET",
+          "Content-type": "applcation/pdf",
+          Referer: referer,
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-User": "?1",
+          "Upgrade-Insecure-Requests": "1",
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+        },
+        // encoding: null,
+        proxy: proxyUrl,
+      };
+
+      const writable = fs.createWriteStream(downloadFile, { flags: "w+" });
+      const stream = request
+        .get(options)
+        .on("response", (response: any) => {
+          console.log("response code on download:", response.statusCode);
+          if (response.statusCode >= 500) {
+            console.log(response.statusCode, " Server error", options.uri);
+          }
+        })
+        .on("error", (err: any) => {
+          console.log("err while requesting for pdf");
+          Helper.proxies = Helper.proxies.filter(x => x !== proxyUrl);
+          rej(err);
+        })
+        .pipe(writable)
+        .on("error", (err: any) => {
+          console.log("err while writing pdf");
+          rej(err);
+        });
+
       stream.on("finish", async () => {
         writable.end();
         const stats = fs.statSync(downloadFile);
@@ -130,34 +133,37 @@ export default class Helper {
     });
   };
 
-  private static getProxy = async (): Promise<string | null> => {
-    if (!Helper.proxies || Helper.proxies.length <= 0) {
-      const options = {
-        uri: "https://fcc-node-server.herokuapp.com/proxy",
-        headers: {
-          "User-Agent": "Request-Promise",
-        },
-        json: true, // Automatically parses the JSON string in the response
-      };
+  private static getProxy = (): Promise<string | null> => {
+    return new Promise<string | null>((res, rej) => {
+      if (!Helper.proxies || Helper.proxies.length <= 0) {
+        const options = {
+          uri: "https://fcc-node-server.herokuapp.com/proxy",
+          headers: {
+            "User-Agent": "Request-Promise",
+          },
+          json: true, // Automatically parses the JSON string in the response
+        };
 
-      await rp(options)
-        .then((resp: any) => {
-          if (resp !== null && Array.isArray(resp) && resp.length > 0) {
-            Helper.proxies = [...resp];
-          }
-        })
-        .catch((err: any): string | null => {
-          console.log("check fccid exists err", err);
-          return null;
-        });
-    }
-
-    if (Helper.proxies.length > 0) {
-      const idx = Math.floor(Math.random() * (Helper.proxies.length - 1) + 0);
-      console.log("pick up proxy", Helper.proxies[idx]);
-      return Helper.proxies[idx];
-    } else {
-      throw new Error("no proxy found");
-    }
+        rp(options)
+          .then((resp: any) => {
+            if (resp !== null && Array.isArray(resp) && resp.length > 0) {
+              Helper.proxies = [...resp];
+              const idx = Math.floor(Math.random() * (Helper.proxies.length - 1) + 0);
+              console.log("pick up proxy", Helper.proxies[idx]);
+              res(Helper.proxies[idx]);
+            } else {
+              rej(new Error("no proxy found"));
+            }
+          })
+          .catch((err: any) => {
+            console.log("check fccid exists err", err);
+            rej(err);
+          });
+      } else {
+        const idx = Math.floor(Math.random() * (Helper.proxies.length - 1) + 0);
+        console.log("pick up proxy", Helper.proxies[idx]);
+        res(Helper.proxies[idx]);
+      }
+    });
   };
 }
